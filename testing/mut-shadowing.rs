fe94884f92bc1d768636adbdcbdd24a65dbbe812
Param Siddharth <contact@paramsid.com>
2021-08-08 13:17:14 +0530

    Test unused mutability and then shadowing

---
+++ testing/mut-shadowing.rs
@@ -0,0 +1,7 @@
+fn main() {
+	let mut spaces = "       ";
+	// spaces = spaces.len(); // This fails
+	let spaces = spaces.len(); // (?) This doesn't, but a warning is shown, saying "spaces" doesn't need to be mutable
+
+	println!("{}", spaces);
+}
