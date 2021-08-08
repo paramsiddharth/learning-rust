676ada674f868cf4a37e6ec82c752c9fc0316edb
Param Siddharth <contact@paramsid.com>
2021-08-08 13:20:20 +0530

    Allow unused

--- testing/mut-shadowing.rs
+++ testing/mut-shadowing.rs
@@ -1,7 +1,7 @@
+#![allow(unused)]
+
 fn main() {
 	let mut spaces = "       ";
 	// spaces = spaces.len(); // This fails
 	let spaces = spaces.len(); // (?) This doesn't, but a warning is shown, saying "spaces" doesn't need to be mutable
-
-	println!("{}", spaces);
 }
