6f786f4cb90d205ebbb2d12537a59c3d80e8e80d
Param Siddharth <contact@paramsid.com>
2022-02-13 19:17:44 +0530

    Add support for command-line arguments
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- 5-fibonacci/src/main.rs
+++ 5-fibonacci/src/main.rs
@@ -1,22 +1,29 @@
 use std::io;
 use std::io::Write;
 use std::process;
+use std::env;
 
 fn main() {
-    print!("Enter the number: ");
-
-    io::stdout().flush().expect("Failed to print!");
-
     let mut num = String::new();
+    
+    if env::args().len() < 2 {
+        print!("Enter the number: ");
+    
+        io::stdout().flush().expect("Failed to print!");
 
-    io::stdin()
-        .read_line(&mut num)
-        .expect("Invalid number!");
+
+        io::stdin()
+            .read_line(&mut num)
+            .expect("Invalid number!");
+    } else {
+        let args: Vec<String> = env::args().collect();
+        num = args[1].to_string();
+    }
 
     let num: u32 = match num.trim().parse() {
         Ok(n) => n,
         Err(_) => {
-            eprintln!("Invalid number!");
+            eprintln!("Invalid number '{}'!", num);
             process::exit(1);
         }
     };
