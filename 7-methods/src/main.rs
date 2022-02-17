dafb7a103b634f1bb47b1fe50698ab68b4fc9be7
Param Siddharth <contact@paramsid.com>
2022-02-17 18:02:38 +0530

    Associated (static) functions (methods)
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- 7-methods/src/main.rs
+++ 7-methods/src/main.rs
@@ -7,10 +7,7 @@ fn main() {
     println!("Permieter: {}", rect_1.perimeter());
     println!("Area: {}", rect_1.area());
 
-    let rect_2 = Rectangle {
-        height: 3 as f32,
-        width: 4 as f32
-    };
+    let rect_2 = Rectangle::square(3 as f32);
 
     println!("Rectangle 1 inside 2: {}", rect_2.can_hold(&rect_1));
     println!("Rectangle 2 inside 1: {}", rect_1.can_hold(&rect_2));
@@ -32,5 +29,12 @@ impl Rectangle {
 
     fn can_hold(&self, rect: &Rectangle) -> bool {
         self.height >= rect.height && self.width >= rect.width
+    }
+
+    fn square(size: f32) -> Rectangle {
+        Rectangle {
+            height: size,
+            width: size
+        }
     }
 }
