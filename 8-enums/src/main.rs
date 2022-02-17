cfd67699314e5699053975af37fa0823a3792d07
Param Siddharth <contact@paramsid.com>
2022-02-17 18:16:07 +0530

    Enums
    Signed-off-by: Param Siddharth <contact@paramsid.com>

---
+++ 8-enums/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let param = Person {
+        name: String::from("Param"),
+        gender: Gender::Male
+    };
+
+    println!("{}", param);
+}
+
+enum Gender {
+    Male,
+    Female
+}
+
+struct Person {
+    name: String,
+    gender: Gender
+}
+
+impl Gender {
+    fn to_string(&self) -> String {
+        match &self {
+            Gender::Male => String::from("Male"),
+            Gender::Female => String::from("Female")
+        }
+    }
+}
+
+impl std::fmt::Display for Person {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Person: {} ({})", self.name, self.gender.to_string())
+    }
+}
