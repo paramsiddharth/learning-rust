c3404e7f160f0f918c38bb56fa4ec4ed81968c79
Param Siddharth <contact@paramsid.com>
2022-03-01 19:42:53 +0530

    Project (1)
    Signed-off-by: Param Siddharth <contact@paramsid.com>

---
+++ 15-project/src/main.rs
@@ -0,0 +1,46 @@
+use std::{env, fs, process};
+use std::error::Error;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    let Config { query, filename } = &config;
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    filename: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Ok(Config { query, filename })
+    }
+}
