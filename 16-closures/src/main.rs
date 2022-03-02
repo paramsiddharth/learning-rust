138d5ef3e2b8cd74df272e055162a8bc959a711d
Param Siddharth <contact@paramsid.com>
2022-03-02 23:05:47 +0530

    Closures (2)
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- 16-closures/src/main.rs
+++ 16-closures/src/main.rs
@@ -9,23 +9,15 @@ fn main() {
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_result = expensive_calculation(intensity);
     if intensity < 25 {
-        println!(
-            "Today, do {} push-ups!",
-            expensive_calculation(intensity)
-        );
-        println!(
-            "Today, do {} sit-ups!",
-            expensive_calculation(intensity)
-        );
+        println!("Today, do {} push-ups!", expensive_result);
+        println!("Today, do {} sit-ups!", expensive_result);
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!(
-                "Today, run for {} minutes!",
-                expensive_calculation(intensity)
-            );
+            println!("Today, run for {} minutes!", expensive_result);
         }
     }
 }
